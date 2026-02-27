603c0f660dd5514c1dd4283325e0ecef3f4d1ba8
Yahya Yasir Koca <yykoca.08@gmail.com>
2026-02-28 00:07:56 +0100

    Update API URL and author ID in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.yasarkoca.xyz/api',
-  authorId: '018e943f-4da5-7c45-8806-6c77361a4178',
+  apiUrl: 'https://api.yasarkoca.com/api',
+  authorId: '019ca14d-eb57-7dab-8a4d-bfb510bcb8c9',
 };
